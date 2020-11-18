1a1edf1619ec63c00ba2b78958676a90e4bc46a5
Bruno Paranhos Silva <62160761+paranhossilva@users.noreply.github.com>
2020-11-18 10:19:57 -0300

    Add files via upload

---
+++ TP01/Ex02/Author.java
@@ -0,0 +1,34 @@
+//Bruno Paranhos Silva  CB3005437
+
+package tp01;
+
+public class Author {
+    private String name, email;
+    private char gender;
+    
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }    
+    
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public char getGender() {
+        return gender;
+    }
+    
+    public Author(String name, String email, char gender){
+        this.name = name;
+        this.email = email;
+        this.gender = gender;
+    }
+    
+    public String toString(){
+        return "Author [ name = " + name + ", email = " + email + ", gender = " + gender + " ]";
+    }
+}
