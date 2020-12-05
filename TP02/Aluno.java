8e4701e0ea6b3f233caa2acc06b3e61199818474
Bruno Paranhos Silva <62160761+paranhossilva@users.noreply.github.com>
2020-12-05 12:36:58 -0300

    Add files via upload

---
+++ TP02/Aluno.java
@@ -0,0 +1,41 @@
+import java.util.UUID;
+
+//Bruno Paranhos Silva  CB3005437
+
+public class Aluno {
+    private String nome, endereco;
+    private int idade;
+    private UUID uuid;
+       
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public UUID getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(UUID uuid) {
+        this.uuid = uuid;
+    }
+}
